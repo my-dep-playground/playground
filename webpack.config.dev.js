0448a43090c8031181fd2dedf4d7af2523a1f4c8
Poorni Amaratunga <poorniamarathunga1997@gmail.com>
2020-12-02 20:33:43 +0530

    Add webpack config files

---
+++ webpack.config.dev.js
@@ -0,0 +1,16 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { default: merge } = require("webpack-merge");
+const superConfig = require('./webpack.config');
+
+module.exports = merge(superConfig,{
+    devServer: {
+        port: 3000
+      },
+    devtool: false,
+    mode: 'development',
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename:'minn.css'
+        })
+    ]
+}) 
